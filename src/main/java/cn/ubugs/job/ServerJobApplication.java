623e80ec2f8c45a821980575059d151b3ef200cf
Ubugs <834602615@qq.com>
2021-12-06 14:50:38 +0800

    初始化

---
+++ src/main/java/cn/ubugs/job/ServerJobApplication.java
@@ -0,0 +1,13 @@
+package cn.ubugs.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerJobApplication.class, args);
+    }
+
+}
